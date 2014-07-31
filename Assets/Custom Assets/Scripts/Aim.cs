0a5159df10aadbeb5823e77596151e28b0c21a75
mimlowe <michaellowe33@eduroam-227-87.ucsc.edu>
2014-07-30 23:46:00 -0700

    Animation & Environment
    Animation (Right Trigger Aims Bottle)
    Added more Environment Assets

---
+++ Assets/Custom Assets/Scripts/Aim.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Fire1") >= 1){
+			animation["Aim"].wrapMode = WrapMode.ClampForever;
+			animation.Play("Aim");
+		} else if (Input.GetAxis("Fire1") <= 0){
+			animation["Aim"].wrapMode = WrapMode.Once;
+			animation.Play("UnAim");
+		}
+	}
+}
