40138de4d560b711eab78b4fbd11b5ea31a25564
Jesse Smith <jessesm24@live.com>
2014-08-02 16:03:28 -0700

    Added transparency to minimap

--- Assets/Custom Assets/Scripts/GUIController.cs
+++ Assets/Custom Assets/Scripts/GUIController.cs
@@ -9,6 +9,7 @@ public class GUIController : MonoBehaviour
 	public Texture minimapIndicator;
 	public float indicatorScale = 1.0f;
 	public float gameworldSize = 100; //size in the ortho minimap camera used to get the minimap texture
+	public float minimapAlpha = 0.75f;
 
 	const int minimapBaseSize = 256;
 	const int indicatorBaseSize = 32;
@@ -18,6 +19,7 @@ public class GUIController : MonoBehaviour
 	Rect minimapLocation;
 	int minimapSize, indicatorSize;
 	GameObject player;
+	Color guiColor = Color.white;
 
 	void Start() {
 		player = GameObject.Find("First Person Controller");
@@ -30,6 +32,9 @@ public class GUIController : MonoBehaviour
 		minimapSize = (int)(minimapBaseSize * screenRatio * minimapScale);
 		indicatorSize = (int)(indicatorBaseSize * screenRatio * indicatorScale);
 		minimapPadding = (int)(minimapPadding * screenRatio);
+
+		guiColor.a = minimapAlpha;
+
 
 		//Top right
 		//minimapLocation = new Rect(screenWidth - minimapSize - minimapPadding, 0 + minimapPadding, minimapSize, minimapSize);
@@ -49,12 +54,18 @@ public class GUIController : MonoBehaviour
 	}
 
 	void OnGUI() {
+		GUI.color = guiColor;
+
+		//Draw minimap
 		GUI.DrawTexture(minimapLocation, minimap, ScaleMode.ScaleToFit);
 
+		//Draw player position indicator
 		Rect indicatorLocation = new Rect(minimapLocation.left + minimapLocation.width / 2 + (player.transform.position.x / gameworldSize * minimapSize / 2) - indicatorSize / 2, minimapLocation.top + minimapLocation.height / 2 - (player.transform.position.z / gameworldSize * minimapSize / 2) - indicatorSize / 2, indicatorSize, indicatorSize);
 		Matrix4x4 backup = GUI.matrix;
 		GUIUtility.RotateAroundPivot(player.transform.eulerAngles.y, new Vector2(indicatorLocation.x + indicatorLocation.width / 2, indicatorLocation.y + indicatorLocation.height / 2));
 		GUI.DrawTexture(indicatorLocation, minimapIndicator, ScaleMode.ScaleToFit);
 		GUI.matrix = backup;
+
+		GUI.color = Color.white;
 	}
 }
