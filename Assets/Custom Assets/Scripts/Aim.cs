0b130e97389eaa58ce10e46a91e715701fcbf4e9
Shane Do <nguyenshane@gmail.com>
2014-08-05 14:44:26 -0700

    Set new inputs, moved Player.cs to windex, add ani
    right click to aim, left click to shoot. New unity in GD.

--- Assets/Custom Assets/Scripts/Aim.cs
+++ Assets/Custom Assets/Scripts/Aim.cs
@@ -10,12 +10,12 @@ public class Aim : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetAxis("Fire1") >= 1){
+	/*	if(Input.GetAxis("Fire1") >= 1){
 			animation["Aim"].wrapMode = WrapMode.ClampForever;
 			animation.Play("Aim");
 		} else if (Input.GetAxis("Fire1") <= 0){
 			animation["Aim"].wrapMode = WrapMode.Once;
 			//animation.Play("UnAim");
-		}
+		}*/
 	}
 }
--- Assets/Custom Assets/Scripts/Player.cs
+++ Assets/Custom Assets/Scripts/Player.cs
@@ -11,14 +11,17 @@ public class Player : MonoBehaviour {
 	public Transform nozzle;
 	public float fireDelay = 0.1f;
 	public float projectileSpeed = 10.0f;
+	protected Animator animator;
 
-	CharacterController character;
+	public CharacterController character;
 	float fireDelayTimer;
 
 
 	// Use this for initialization
 	void Start () {
+		if(!character)
 		character = GetComponent<CharacterController>();
+		animator = GetComponent<Animator>();
 		fireDelayTimer = 0;	
 	}
 	
@@ -26,11 +29,23 @@ public class Player : MonoBehaviour {
 	void Update () {
 		if (fireDelayTimer >= 0) fireDelayTimer -= Time.deltaTime;
 
+		if(animator)
+		{
+			//get the current state
+			AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+
+		}
+
+		if (Input.GetAxis("Aim") > 0){
+			animator.SetBool("Aim", true );
 		if (Input.GetAxis("Fire1") > 0 && fireDelayTimer <= 0) {
 			fireDelayTimer = fireDelay;
-
+			Gamepad.SetVibration(0,testA,testB);
 			Projectile newProjectile = ((Transform)Instantiate(projectile, nozzle.position + (transform.forward * 0.5f), Quaternion.identity)).gameObject.GetComponent<Projectile>();
 			newProjectile.rigidbody.velocity = character.velocity + transform.forward * projectileSpeed;
 		}
+		}
+		else animator.SetBool("Aim", false );
 	}
 }
