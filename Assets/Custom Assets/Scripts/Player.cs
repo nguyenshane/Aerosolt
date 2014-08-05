8140c59a01e022c8fc963ebfa3ef0af4d46b1c91
Jesse Smith <jessesm24@live.com>
2014-08-05 12:40:33 -0700

    Improved projectile firing

--- Assets/Custom Assets/Scripts/Player.cs
+++ Assets/Custom Assets/Scripts/Player.cs
@@ -9,28 +9,37 @@ public class Player : MonoBehaviour {
 
 	public Transform projectile;
 	public Transform nozzle;
-	public float fireDelay = 0.1f;
-	public float projectileSpeed = 10.0f;
+	public float fireRate = 120.0f; //projectiles per second
+	public float projectileSpeed = 12.0f;
+	public float spread = 0.3f;
+
+	const float velocityDeviation = 0.2f;
 
 	CharacterController character;
+	float fireDelay;
 	float fireDelayTimer;
 
 
 	// Use this for initialization
 	void Start () {
 		character = GetComponent<CharacterController>();
-		fireDelayTimer = 0;	
+		fireDelay = 1.0f / fireRate;
+		fireDelayTimer = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (fireDelayTimer >= 0) fireDelayTimer -= Time.deltaTime;
+		else if (Input.GetAxis("Fire1") > 0) {
+			while (fireDelayTimer < fireDelay) {
+				fireDelayTimer += fireDelay; //allows the correct number of projectiles to be fired per second regardless of framerate
 
-		if (Input.GetAxis("Fire1") > 0 && fireDelayTimer <= 0) {
-			fireDelayTimer = fireDelay;
+				Vector3 direction = transform.forward + (Random.onUnitSphere * spread);
+				direction.Normalize();
 
-			Projectile newProjectile = ((Transform)Instantiate(projectile, nozzle.position + (transform.forward * 0.5f), Quaternion.identity)).gameObject.GetComponent<Projectile>();
-			newProjectile.rigidbody.velocity = character.velocity + transform.forward * projectileSpeed;
+				Projectile newProjectile = ((Transform)Instantiate(projectile, nozzle.position + (transform.forward * 0.5f), Quaternion.identity)).gameObject.GetComponent<Projectile>();
+				newProjectile.rigidbody.velocity = character.velocity + (direction * projectileSpeed * (1 + Random.Range(-velocityDeviation, velocityDeviation)));
+			}
 		}
 	}
 }
