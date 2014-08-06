7a56d6f2233bc0dd4232ed1c0454da65e30af698
Jesse Smith <jessesm24@live.com>
2014-08-05 17:11:33 -0700

    Fixing merge confict

--- Assets/Custom Assets/Scripts/Player.cs
+++ Assets/Custom Assets/Scripts/Player.cs
@@ -9,50 +9,43 @@ public class Player : MonoBehaviour {
 
 	public Transform projectile;
 	public Transform nozzle;
-	public float fireDelay = 0.1f;
 	protected Animator animator;
-
-	public CharacterController character;
 	public float fireRate = 120.0f; //projectiles per second
 	public float projectileSpeed = 12.0f;
 	public float spread = 0.3f;
 
 	const float velocityDeviation = 0.2f;
 
+	CharacterController character;
+	float fireDelay;
 	float fireDelayTimer;
 
 
 	// Use this for initialization
 	void Start () {
-		if(!character)
-		character = GetComponent<CharacterController>();
+		if(!character) character = GetComponent<CharacterController>();
 		animator = GetComponent<Animator>();
-		fireDelayTimer = 0;	
+		
 		fireDelay = 1.0f / fireRate;
+		fireDelayTimer = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (fireDelayTimer >= 0) fireDelayTimer -= Time.deltaTime;
-		else if (Input.GetAxis("Aim") > 0){
-			animator.SetBool("Aim", true );
-		
-		if (Input.GetAxis("Fire1") > 0) {
+		else if (Input.GetAxis("Fire1") > 0) {
 			while (fireDelayTimer < fireDelay) {
 				fireDelayTimer += fireDelay; //allows the correct number of projectiles to be fired per second regardless of framerate
 
+				Vector3 direction = transform.forward + (Random.onUnitSphere * spread);
+				direction.Normalize();
 
-		
-		if (Input.GetAxis("Fire1") > 0 && fireDelayTimer <= 0) {
-			fireDelayTimer = fireDelay;
-			Projectile newProjectile = ((Transform)Instantiate(projectile, nozzle.position + (transform.forward * 0.5f), Quaternion.identity)).gameObject.GetComponent<Projectile>();
-			Vector3 direction = transform.forward + (Random.onUnitSphere * spread);
-			direction.Normalize();
-			newProjectile.rigidbody.velocity = character.velocity + (direction * projectileSpeed * (1 + Random.Range(-velocityDeviation, velocityDeviation)));
+				Projectile newProjectile = ((Transform)Instantiate(projectile, nozzle.position + (transform.forward * 0.5f), Quaternion.identity)).gameObject.GetComponent<Projectile>();
+				newProjectile.rigidbody.velocity = character.velocity + (direction * projectileSpeed * (1 + Random.Range(-velocityDeviation, velocityDeviation)));
 			}
-		  }
 		}
+	
+		if (Input.GetAxis("Aim") > 0) animator.SetBool("Aim", true);
+		else animator.SetBool("Aim", false);
 	}
-		else animator.SetBool("Aim", false );
-  }
 }
