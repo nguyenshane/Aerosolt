b78c881161ebc41b3487b80ba46688a1c9bc2ca2
Shane Do <nguyenshane@gmail.com>
2014-08-07 17:17:27 -0700

    Updated prefab

---
+++ Assets/Custom Assets/Scripts/WaterLevel.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+	/*var mesh : Mesh = GetComponent(MeshFilter).mesh;
+	var triangles = mesh.GetTriangles(1);
+	var vertices : Vector3[] = mesh.vertices;
+	var normals : Vector3[] = mesh.normals;
+	Debug.Log(triangles.);
+	for (var i = 0; i < vertices.Length; i++)
+		vertices[i] += normals[i] * Mathf.Sin(Time.time);
+
+	mesh.vertices = vertices;*/
+}
