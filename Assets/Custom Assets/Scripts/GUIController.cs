04d9ffd9dc2d325bd323219c36509b65d747915f
Jesse Smith <jessesm24@live.com>
2014-08-08 18:54:07 -0700

    Added runtime-generated minimap (disabled by default since it doesn't render terrain), updated regular minimap image and unity file

--- Assets/Custom Assets/Scripts/GUIController.cs
+++ Assets/Custom Assets/Scripts/GUIController.cs
@@ -14,13 +14,18 @@ public class GUIController : MonoBehaviour
 {
 	public bool oculusEnabled = false;
 	public bool showFramerate = false;
+	public bool dynamicMinimap = false;
+
+	public GUIStyle label;
+
 	public Texture minimap;
 	public float minimapScale = 1.0f;
 	public int minimapPadding = 32;
 	public float minimapAlpha = 0.5f;
+
 	public Texture minimapIndicator;
 	public float indicatorScale = 1.0f;
-	public float gameworldSize = 100; //size in the ortho minimap camera used to get the minimap texture
+	
 	public Texture reticule;
 	public float reticuleScale = 1.0f;
 
@@ -30,17 +35,38 @@ public class GUIController : MonoBehaviour
 
 	int screenWidth, screenHeight;
 	float screenRatio;
+
 	Rect minimapLocation;
 	int minimapSize, indicatorSize, reticuleSize;
-	GameObject player;
 	Color minimapTint = Color.white;
 	bool showMinimap = true;
-	float minimapToggleDelay = 0.25f;
+	float gameworldSize; //size in the ortho minimap camera used to get the minimap texture
+
+	GameObject player;
 
 
 	void Start() {
 		player = GameObject.Find("First Person Controller");
 
+		Transform minimapCamera = transform.Find("Minimap Camera");
+		Camera mmCameraComp = minimapCamera.gameObject.GetComponent<Camera>();
+		gameworldSize = mmCameraComp.orthographicSize;
+
+		if (dynamicMinimap) {
+			mmCameraComp.targetTexture = new RenderTexture(1024, 1024, 16, RenderTextureFormat.ARGB32);
+			RenderTexture currentRT = RenderTexture.active;
+			RenderTexture.active = mmCameraComp.targetTexture;
+			mmCameraComp.Render();
+			Texture2D mm = new Texture2D(mmCameraComp.targetTexture.width, mmCameraComp.targetTexture.height);
+			mm.ReadPixels(new Rect(0, 0, mmCameraComp.targetTexture.width, mmCameraComp.targetTexture.height), 0, 0);
+			mm.Apply();
+			minimap = mm as Texture;
+			RenderTexture.active = currentRT;
+		}
+
+		minimapCamera.gameObject.SetActive(false);
+		
+		
 		minimapTint.a = minimapAlpha;
 
 		if (!oculusEnabled) {
@@ -53,6 +79,8 @@ public class GUIController : MonoBehaviour
 			indicatorSize = (int)(indicatorBaseSize * screenRatio * indicatorScale);
 			reticuleSize = (int)(reticuleBaseSize * screenRatio * reticuleScale);
 			minimapPadding = (int)(minimapPadding * screenRatio);
+
+			label.fontSize = (int)(label.fontSize * screenRatio);
 
 			//Standard camera minimap locations
 			
@@ -105,6 +133,15 @@ public class GUIController : MonoBehaviour
 	void OnGUI() {
 		if (oculusEnabled) return;
 
+		//Draw reticule
+		GUI.DrawTexture(new Rect(screenWidth / 2 - reticuleSize / 2, screenHeight / 2 - reticuleSize / 2, reticuleSize, reticuleSize), reticule, ScaleMode.ScaleToFit);
+
+		//Draw health
+		GUI.Label(new Rect((int)screenWidth / 3, (int)screenHeight / 3, 60 * screenRatio, 20 * screenRatio), player.GetComponentInChildren<Player>().getHP().ToString(), label);
+		
+		//Draw framerate
+		if (showFramerate) GUI.Label(new Rect(32 * screenRatio, 32 * screenRatio, 400 * screenRatio, 400 * screenRatio), (1 / Time.deltaTime).ToString());
+
 		if (showMinimap) {
 			GUI.color = minimapTint;
 			//Draw minimap
@@ -118,18 +155,22 @@ public class GUIController : MonoBehaviour
 			GUI.DrawTexture(indicatorLocation, minimapIndicator, ScaleMode.ScaleToFit);
 			GUI.matrix = backup;
 		}
-
-		//Draw reticule
-		GUI.DrawTexture(new Rect(screenWidth / 2 - reticuleSize / 2, screenHeight / 2 - reticuleSize / 2, reticuleSize, reticuleSize), reticule, ScaleMode.ScaleToFit);
-
-		//Draw framerate
-		if (showFramerate) GUI.Label(new Rect(32 * screenRatio, 32 * screenRatio, 400 * screenRatio, 400 * screenRatio), (1 / Time.deltaTime).ToString());
 	}
 
 
 	//Called in OVRMainMenu.cs
 	public void OculusGUI(ref OVRGUI GuiHelper) {
 		if (!oculusEnabled) return;
+
+		//Reticule
+		GuiHelper.StereoDrawTexture((int)(screenWidth / 2 - reticuleSize / 2), (int)(screenHeight / 2 - reticuleSize / 2), (int)reticuleSize, (int)reticuleSize, ref reticule, Color.white);
+		
+		//Health
+		string hp = player.GetComponentInChildren<Player>().getHP().ToString();
+		GuiHelper.StereoBox((int)screenWidth / 3, (int)screenHeight / 3, 60, 20, ref hp, Color.red);
+		
+		//Draw framerate
+		if (showFramerate) GUI.Label(new Rect(600, 240, 400, 400), (1 / Time.deltaTime).ToString());
 
 		if (showMinimap) {
 			GUI.color = minimapTint;
@@ -146,15 +187,5 @@ public class GUIController : MonoBehaviour
 			GuiHelper.StereoDrawTexture((int)(indicatorLocation.x), (int)(indicatorLocation.y), (int)indicatorLocation.width, (int)indicatorLocation.height, ref minimapIndicator, Color.white);
 			//GUI.matrix = backup;
 		}
-
-		//Reticule
-		GuiHelper.StereoDrawTexture((int)(screenWidth / 2 - reticuleSize / 2), (int)(screenHeight / 2 - reticuleSize / 2), (int)reticuleSize, (int)reticuleSize, ref reticule, Color.white);
-
-		//Health
-		string hp = player.GetComponentInChildren<Player>().getHP().ToString();
-		GuiHelper.StereoBox((int)screenWidth / 3, (int)screenHeight / 3, 60, 20, ref hp, Color.red);
-
-		//Draw framerate
-		if (showFramerate) GUI.Label(new Rect(600, 240, 400, 400), (1 / Time.deltaTime).ToString());
 	}
 }
