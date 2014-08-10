cc9e182338d43ca7e177fb373ad961a6ae33a656
Jesse Smith <jessesm24@live.com>
2014-08-10 11:49:29 -0700

    Improved AI some more

--- Assets/Custom Assets/Scripts/AI.cs
+++ Assets/Custom Assets/Scripts/AI.cs
@@ -7,10 +7,12 @@ using System.Collections;
 
 public class AI : MonoBehaviour {
 
-	public float reactionTime = 0.5f;
+	public float targetAcquisitionTime = 0.5f;
+	public float reactionTime = 0.2f;
 
+	float targetAcquisitionTimer;
 	float reactionTimer;
-	//bool hasTarget = false; //Not needed right now
+	bool hasTarget = false;
 	bool returning = true;
 	Vector3 returnPosition;
 	Vector3 targetDir;
@@ -25,22 +27,23 @@ public class AI : MonoBehaviour {
 		stats = GetComponent<Enemy>();
 		hitinfo = new RaycastHit();
 
+		targetAcquisitionTimer = targetAcquisitionTime;
 		reactionTimer = reactionTime;
 		returnPosition = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (reactionTimer >= 0) reactionTimer -= Time.deltaTime;
+		if (targetAcquisitionTimer >= 0) targetAcquisitionTimer -= Time.deltaTime;
 		else {
-			reactionTimer = Random.Range(reactionTime / 2, reactionTime * 1.5f);
-			targetDir = target.position - transform.position + (Random.insideUnitSphere * target.collider.bounds.size.x * 0.9f);
+			targetAcquisitionTimer = Random.Range(targetAcquisitionTime / 2, targetAcquisitionTime * 1.5f);
+			targetDir = target.position - transform.position + (Random.insideUnitSphere * target.collider.bounds.size.x * 0.7f);
 			Physics.Raycast(transform.position, targetDir, out hitinfo);
 
 			if (hitinfo.collider != null) {
 				if (hitinfo.collider.tag == "Player") {
 					//Follow target
-					//hasTarget = true;
+					hasTarget = true;
 					returning = false;
 				} else {
 					//Mirror the previous raycast across the vector pointing to the center of the player and try again
@@ -49,19 +52,25 @@ public class AI : MonoBehaviour {
 
 					if (hitinfo.collider.tag == "Player") {
 						//Follow target
-						//hasTarget = true;
+						hasTarget = true;
 						returning = false;
 					} else {
-						//hasTarget = false;
+						hasTarget = false;
 
 						if (hitinfo.distance <= transform.localScale.x + Random.Range(0.2f, 0.7f) + Mathf.Pow(rigidbody.velocity.magnitude, 2) / (2 * stats.acceleration)) {
 							//About to run into something that isn't the player, begin returning
-							//hasTarget = false;
+							hasTarget = false;
 							returning = true;
 						}
 					}
 				}
 			}
+		}
+
+		if (reactionTimer >= 0) reactionTimer -= Time.deltaTime;
+		else if (!returning && hasTarget) {
+			reactionTimer = Random.Range(reactionTime / 2, reactionTime * 1.5f);
+			targetDir = target.position - transform.position + (Random.insideUnitSphere * target.collider.bounds.size.x * 0.7f);
 		}
 
 		if (!returning) {
