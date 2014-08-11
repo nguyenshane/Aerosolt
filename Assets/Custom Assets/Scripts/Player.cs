2906bed433a14269dea9c492e4b917f75ef4f39d
jessesm <jeamsmit@ucsc.edu>
2014-08-11 16:48:42 -0700

    Changed firing mechanics

--- Assets/Custom Assets/Scripts/Player.cs
+++ Assets/Custom Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour {
 	
 	public static float hitpoints = 100.0f;
 	public static float ammunition = 100.0f;
+	public float burstRate = 3.0f; //trigger presses per second for constant fire
 	public float fireRate = 960.0f; //projectiles per second
 	public float projectileSpeed = 12.0f;
 	public float spread = 0.3f;
@@ -18,10 +19,12 @@ public class Player : MonoBehaviour {
 	public Transform nozzle;
 
 	const float velocityDeviation = 0.2f;
+	const float deadzone = 0.1f;
 
 	protected Animator animator;
 	public CharacterController character;
-	float fireDelay, fireDelayTimer;
+	float fireDelay, fireDelayTimer, burstDelay, burstDelayTimer;
+	bool firing, triggerReset;
 	static float hp, ammo;
 	static bool initialized = false;
 	
@@ -40,16 +43,32 @@ public class Player : MonoBehaviour {
 			initialized = true;
 		}
 
+		burstDelay = 1.0f / burstRate;
+		burstDelayTimer = 0.0f;
 		fireDelay = 1.0f / fireRate;
-		fireDelayTimer = 0;
-
+		fireDelayTimer = 0.0f;
+		firing = triggerReset = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (burstDelayTimer >= 0) burstDelayTimer -= Time.deltaTime;
+		else firing = false;
+
+		float fireInput = Input.GetAxis("Fire1");
+		if (fireInput <= deadzone) triggerReset = true;
+
+		if (burstDelayTimer <= 0 && !firing && ammo >= ammoConsumption) {
+			if (triggerReset && fireInput > deadzone) {
+				firing = true;
+				triggerReset = false;
+				burstDelayTimer = burstDelay;
+				audio.Play();
+			}
+		}
+
 		if (fireDelayTimer >= 0) fireDelayTimer -= Time.deltaTime;
-		else if (Input.GetAxis ("Fire1") > 0) {
-			audio.Play ();
+		else if (firing) {
 			while (fireDelayTimer < fireDelay && ammo >= ammoConsumption) {
 				fireDelayTimer += fireDelay; //allows the correct number of projectiles to be fired per second regardless of framerate
 				
@@ -61,7 +80,7 @@ public class Player : MonoBehaviour {
 
 				ammo -= ammoConsumption;
 			}
-		} 
+		}
 
 		if (Input.GetAxis ("Aim") > 0) animator.SetBool ("Aim", true);
 		else animator.SetBool("Aim", false);
--- Assets/Custom Assets/Scripts/StartGame.cs
+++ Assets/Custom Assets/Scripts/StartGame.cs
@@ -5,6 +5,7 @@ public class StartGame : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.tag == "Player") {
+			Player.addAmmo(Player.ammunition - Player.getAmmo());
 			Application.LoadLevel(Application.loadedLevel+1);
 		}
 	}
